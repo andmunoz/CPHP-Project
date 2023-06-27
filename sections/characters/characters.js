4fdf28d111a0533c2620d750eb5ac79fd0b744c5
Andrés Muñoz O <andmunoz@moit.cl>
2023-06-27 01:42:17 -0400

    add sections, get values from db and show someones

--- sections/characters/characters.js
+++ sections/characters/characters.js
@@ -42,12 +42,70 @@ function closeCharacterList(){
 function fillCharacterSheet(id){
     actualCharacter = characterList[id];
     console.log(actualCharacter);
-    $('#nombre').html(actualCharacter['nombre']);
-    $('#jugador').html(actualCharacter['jugador']);
-    $('#rol').html(actualCharacter['rol']);
-    $('#fecha_nacimiento').html(actualCharacter['fecha_nacimiento']);
-    $('#estatura').html(actualCharacter['estatura'] + ' mts');
-    $('#peso').html(actualCharacter['peso'] + ' kgs');
-    $('#experiencia').html(actualCharacter['experiencia'] + ' pp');
-    $('#estado').html(actualCharacter['estado']);
+
+    /* Load general information of character */ 
+    $('#nombre').val(actualCharacter['nombre']);
+    $('#jugador').val(actualCharacter['jugador']);
+    $('#rol').val(actualCharacter['rol']);
+    $('#fecha_nacimiento').val(actualCharacter['fecha_nacimiento']);
+    $('#estatura').val(actualCharacter['estatura'] + ' mts');
+    $('#peso').val(actualCharacter['peso'] + ' kgs');
+    $('#experiencia').val(actualCharacter['experiencia'] + ' pp');
+    $('#estado').val(actualCharacter['estado']);
+
+    /* Load attribute values of character */
+    const attributes = { 'INT': 'inteligencia', 
+                         'REF': 'reflejos', 
+                         'TEC': 'tecnica',
+                         'FRI': 'frialdad',
+                         'ATR': 'atractivo',
+                         'SUE': 'suerte',
+                         'MOV': 'movimiento',
+                         'TCO': 'tipo_corporal', 
+                         'EMP': 'empatia' };
+    $.each(actualCharacter['atributos'], function(id, value){
+        $('#' + id).val(value);
+    });
+
+    /* Load abilities values of character */
+    let html = '';
+    let number = 0;
+    $.each(actualCharacter['capacidad_especial'], function(id, ability){
+        number++;
+        html += '<div class="text-center"><label>Especial</label></div>';
+        html += '<div><input type="text" class="grid-value" id="especial_nombre_' + number + '" value="' + ability.nombre + '"/></div>';
+        html += '<div><input type="text" class="grid-value text-center" id="especial_valor' + number + '" value="' + ability.valor + '"/></div>';
+        html += '<div></div>';
+    });
+    number = 0;
+    $.each(actualCharacter['habilidades_principales'], function(id, ability){
+        number++;
+        html += '<div><input type="text" class="grid-value text-center" id="habilidad_atributo_' + number + '" value="' + ability.atributo + '"/></div>';
+        html += '<div><input type="text" class="grid-value" id="habilidad_nombre_' + number +  '" value="' + ability.nombre + '"/></div>';
+        html += '<div><input type="text" class="grid-value text-center" id="habilidad_valor_' + number +  '" value="' + ability.valor + '"/></div>';
+        html += '<div class="text-center"><label>+' + (parseInt(actualCharacter['atributos'][attributes[ability.atributo]]) + parseInt(ability.valor)) + '</label></div>';
+    });
+    $.each(actualCharacter['habilidades_secundarias'], function(id, ability){
+        number++;
+        html += '<div><input type="text" class="grid-value text-center" id="habilidad_atributo_' + number + '" value="' + ability.atributo + '"/></div>';
+        html += '<div><input type="text" class="grid-value" id="habilidad_nombre_' + number +  '" value="' + ability.nombre + '"/></div>';
+        html += '<div><input type="text" class="grid-value text-center" id="habilidad_valor_' + number +  '" value="' + ability.valor + '"/></div>';
+        html += '<div class="text-center"><label>+' + (parseInt(actualCharacter['atributos'][attributes[ability.atributo]]) + parseInt(ability.valor)) + '</label></div>';
+    });
+    $('#abilities-block-content').html(html);
+
+    /* Leave readonly data */
+    $('.grid-value').attr('readonly', true);
 }
+
+$(document).ready(function(){
+    $('#general-block-title').click(function(){
+        $('#general-block-content').slideToggle(1000);
+    });   
+    $('#attributes-block-title').click(function(){
+        $('#attributes-block-content').slideToggle(1000);
+    });
+    $('#abilities-block-title').click(function(){
+        $('#abilities-block-content').slideToggle(1000);
+    });
+});
